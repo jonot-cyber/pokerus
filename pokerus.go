1fc2c02b249711ee153b086362ec09594b698f1b
Joseph Tessitore <joetgamerman3@gmail.com>
2022-11-19 00:26:57 -0500

    Fixed some bugs

--- pokerus.go
+++ pokerus.go
@@ -1,11 +1,13 @@
 package pokerus
 
 import (
-    "fmt"
-    "math/rand"
+	"fmt"
+	"math/rand"
+    "time"
 )
 
 func init() {
+    rand.Seed(time.Now().UnixNano())
     if rand.Int() % 65_536 < 3 {
         fmt.Println("Your gopher has pokérus!")
     }
